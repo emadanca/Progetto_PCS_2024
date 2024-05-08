aac98641839a9218e8137c48436292acc1fd4362
unknown <s297868@studenti.polito.it>
2024-05-08 17:47:11 +0200

    Modifiche

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,4 +1,26 @@
+#include "Utils.hpp"
+#include <sstream>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
 int main()
 {
-  return 0;
+    // Lettura dei dati DFN da un file
+    vector<Fracture> fractures;
+    readDFN("DFN/FR3_data.txt", fractures);
+
+    // Calcolo delle tracce
+    vector<Trace> traces = findTrace(fractures);
+
+    // Classificazione delle tracce come passanti o non-passanti
+    classifyTraces(traces, fractures);
+
+    // Ordinamento delle tracce per lunghezza
+    sortTracesByLength(traces);
+
+    // Stampa delle tracce su un file
+    printTracesToFile(traces, "traces.txt");
+
+    return 0;
 }
--- Project/src/Utils.hpp
+++ Project/src/Utils.hpp
@@ -1,17 +1,60 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include <iostream>
+#include <vector>
 #include <fstream>
-#include <sstream>
+#include <string>
+
+using namespace std; // Utilizzo dello spazio dei nomi standard
+
+// Definizione della struttura per rappresentare un punto tridimensionale
+struct Point3D
+{
+    double x;
+    double y;
+    double z;
+};
+
+// Definizione della struttura per rappresentare una frattura
+struct Fracture
+{
+    int id;
+    vector<Point3D> vertices; // Vettore dei vertici
+};
+
+// Definizione della struttura per rappresentare una traccia
+struct Trace
+{
+    int id;
+    int fracture_id1;
+    int fracture_id2;
+    Point3D start; // Punto di inizio
+    Point3D end;   // Punto di fine
+};
+
+// Dichiarazione delle funzioni
+
+// Funzione per leggere i dati dal file DFN e creare un vettore fatto di fratture che sono caratterizzare da id e coordinate
+
+void readDFN(const string& filename, vector<Fracture>& fractures); // Funzione per leggere i dati DFN da un file
+
+bool segmentIntersection (const Point3D& p1, const Point3D& p2, const Point3D& q1, const Point3D& q2 );
+
+vector<Trace> findTrace(const vector<Fracture>& fractures);
+
+double calculateTraceLength(const Trace& trace);
+
+void sortTracesByLength(vector<Trace>& traces); // Funzione per ordinare le tracce per lunghezza
+
+void classifyTraces(vector<Trace>& traces, const vector<Fracture>& fractures); // Funzione per classificare le tracce come passanti o non-passanti
 
 
-// Funzione per leggere i dati dal file DFN per importare:
-// -il numero di fratture
-// -Id frattura
-// -numero vertici
-// -elenco dei vertici (3D)
-bool ImportPolygons(const std::string& fileName);
+void printTracesToFile(const vector<Trace>& traces, const string& filename); // Funzione per stampare le tracce su un file
+
+void printClassifiedTracesToFile(const vector<vector<Trace>>& passanteTraces, const vector<vector<Trace>>& nonPassanteTraces, const string& filename); // Funzione per stampare le tracce classificate su un file
 
 
-#endif
+
+
+#endif // UTILS_HPP
+
