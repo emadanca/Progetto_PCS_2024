0ff35793e948731b06f48d0e8d87c2a7256ee9a6
unknown <s297868@studenti.polito.it>
2024-05-22 19:48:09 +0200

    modifiche

--- Project/main.cpp
+++ Project/main.cpp
@@ -4,23 +4,129 @@
 #include <cmath>
 #include <algorithm>
 
+using namespace std;
+using namespace DFNLibrary;
+
 int main()
 {
-    // Lettura dei dati DFN da un file
+
+    // Creazione delle fratture di esempio
+    Fracture f1;
+    f1.idFracture= 0;
+    f1.numVertices = 4;
+    Point3D A;
+    A.x = 0.0;
+    A.y = 0.0;
+    A.z = 0.0;
+
+    Point3D B;
+    B.x = 1;
+    B.y = 0;
+    B.z = 0;
+
+    Point3D vettorialeAB = crossProduct(A,B);
+    cout << "vettoriale ab : " << vettorialeAB << endl;
+    Point3D C;
+    C.x = 1;
+    C.y = 1;
+    C.z = 0;
+
+    Point3D D;
+    D.x = 0;
+    D.y = 1;
+    D.z = 0;
+
+    f1.vertices.push_back(A);
+    f1.vertices.push_back(B);
+    f1.vertices.push_back(C);
+    f1.vertices.push_back(D);
+
+    Fracture f2;
+    f2.idFracture= 0;
+    f2.numVertices = 4;
+
+    Point3D E;
+    E.x = 0.8;
+    E.y = 0;
+    E.z = -0.1;
+
+    Point3D F;
+    F.x = 0.8;
+    F.y = 0;
+    F.z = 0.29999999;
+
+    Point3D G;
+    G.x = 0.8;
+    G.y = 1;
+    G.z = 0.29999999;
+
+    Point3D H;
+    H.x = 0.8;
+    H.y = 1;
+    H.z = -0.1;
+
+    f2.vertices.push_back(E);
+    f2.vertices.push_back(F);
+    f2.vertices.push_back(G);
+    f2.vertices.push_back(H);
+
+    Point3D normal1 = crossProduct(f1.vertices[0], f1.vertices[1], f1.vertices[2]);
+    cout << "normale 1" << normal1 << endl;
+
+    Point3D normal2 = crossProduct(f2.vertices[0], f2.vertices[1], f2.vertices[2]);
+    cout << "normale 2" << normal2 << endl;
+    double tol = 1.e-16;
+
     vector<Fracture> fractures;
-    readDFN("DFN/FR3_data.txt", fractures);
+    vector<Trace> traces;
+    array<bool,2> onThePlane;
 
-    // Calcolo delle tracce
-    vector<Trace> traces = findTrace(fractures);
+    // Leggi i dati DFN dal file
+    readDFN("DFN/FR10_data.txt", fractures);
+    cout << "size fractures " << fractures.size() << endl;
 
-    // Classificazione delle tracce come passanti o non-passanti
-    classifyTraces(traces, fractures);
+    for (size_t i = 0; i < fractures.size(); i++) {
+        Fracture f = fractures[i];
+        cout << "Fracture " << i << " vertices:" << endl;
+        cout << f.vertices.size() << endl;
+        for (const auto& vertex : f.vertices) {
+            cout << vertex << endl;
+        }
 
-    // Ordinamento delle tracce per lunghezza
-    sortTracesByLength(traces);
+    for (size_t i = 0; i < fractures.size(); ++i) {
+        for (size_t j = i +1; j < fractures.size(); ++j) {
+            const Fracture& f1 = fractures[i];
+            // cout << "VERTICI VERI : " << f1.vertices[0] << ";" << f1.vertices[1] << ";" << f1.vertices[2] << endl;
+            const Fracture& f2 = fractures[j];
 
-    // Stampa delle tracce su un file
+            // Vettore per memorizzare gli intersectionPoints per questa coppia di fratture
+            vector<Point3D> intersectionPoints(4);
+            // cout << "ciao" << endl;
+
+            // Trova gli intersectionPoints per questa coppia di fratture
+            bool intersectionFound = findIntersection2(f1, f2, intersectionPoints, tol, onThePlane);
+            // cout << "ciao" << endl;
+            /*
+            cout << " intersection point delle fratture " << i << "e" <<j << endl;
+            cout << intersectionPoints[0] << endl;
+            cout << intersectionPoints[1] << endl;
+            cout << intersectionPoints[2] << endl;
+            cout << intersectionPoints[3] << endl;*/
+        }
+    }
+
+    //list<Trace> tracesList; // Creo il vettore che temporaneo di tracce
+    vector<Trace> traces; // Creo il vettore definitivo
+
+    // Trova le tracce
+    traces = findTrace(fractures, 2.2e-16);
+
+    // Stampa le tracce su file
     printTracesToFile(traces, "traces.txt");
+    sortAndDivideTracesByFracture(traces, "tracesSorted.txt");
 
     return 0;
+
 }
+}
+
--- Project/src/Utils.hpp
+++ Project/src/Utils.hpp
@@ -4,8 +4,11 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <array>
 
-using namespace std; // Utilizzo dello spazio dei nomi standard
+using namespace std; // Utilizzo dello spazio dei nomi standarD
+
+namespace DFNLibrary {
 
 // Definizione della struttura per rappresentare un punto tridimensionale
 struct Point3D
@@ -13,48 +16,58 @@ struct Point3D
     double x;
     double y;
     double z;
+
+    // Default constructor
+    Point3D() : x(0.0), y(0.0), z(0.0) {}
+
+    Point3D(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+
 };
+
+ostream& operator << (ostream& os, const Point3D& c);
+Point3D operator-(const Point3D& p1, const Point3D& p2);
+Point3D operator+(const Point3D& p1, const Point3D& p2);
+bool operator==(const Point3D& p1, const Point3D& p2);
+Point3D crossProduct(const Point3D& A, const Point3D& B, const Point3D& C);
+double calculateD(const Point3D& normal, const Point3D& pointOnPlane);
+Point3D crossProduct(const Point3D& v1, const Point3D& v2);
+double distance(const Point3D& A, const Point3D& B);
+double dotProduct(const Point3D& u, const Point3D& v);
 
 // Definizione della struttura per rappresentare una frattura
 struct Fracture
 {
-    int id;
+    int idFracture;
+    int numVertices;
+    Point3D normal; // normale del piano contenente la frattura
+    Point3D center; // centrometro della frattura
     vector<Point3D> vertices; // Vettore dei vertici
 };
 
 // Definizione della struttura per rappresentare una traccia
 struct Trace
 {
-    int id;
+    int idTrace;
     int fracture_id1;
     int fracture_id2;
+    array<bool,2> Tips; // Indica se la traccia è passante (false) o non-passante (true). La dimensione è due perchè si riferisce a due poligoni
+    double length; // Lunghezza della traccia
     Point3D start; // Punto di inizio
     Point3D end;   // Punto di fine
+    vector<Point3D> extremes; // Vettore di punti di intersezione
 };
-
 // Dichiarazione delle funzioni
+void readDFN(const string& filename, vector<Fracture>& fractures);
 
-// Funzione per leggere i dati dal file DFN e creare un vettore fatto di fratture che sono caratterizzare da id e coordinate
+//#################### BOUNDINGBOX ######################
+bool areFracturesFarApart(const Fracture& fracture1, const Fracture& fracture2);
 
-void readDFN(const string& filename, vector<Fracture>& fractures); // Funzione per leggere i dati DFN da un file
-
-bool segmentIntersection (const Point3D& p1, const Point3D& p2, const Point3D& q1, const Point3D& q2 );
-
-vector<Trace> findTrace(const vector<Fracture>& fractures);
-
-double calculateTraceLength(const Trace& trace);
-
-void sortTracesByLength(vector<Trace>& traces); // Funzione per ordinare le tracce per lunghezza
-
-void classifyTraces(vector<Trace>& traces, const vector<Fracture>& fractures); // Funzione per classificare le tracce come passanti o non-passanti
-
-
-void printTracesToFile(const vector<Trace>& traces, const string& filename); // Funzione per stampare le tracce su un file
-
-void printClassifiedTracesToFile(const vector<vector<Trace>>& passanteTraces, const vector<vector<Trace>>& nonPassanteTraces, const string& filename); // Funzione per stampare le tracce classificate su un file
-
-
-
-
+//############### INTERSEZIONI FRATTURE ##############
+Point3D intersectionPlaneLine(const Point3D& coefficienti, const double d, const Point3D& A, const Point3D& B);
+bool findIntersection2(const Fracture& f1, const Fracture& f2, vector<Point3D>& intersectionPoints, double tol, array<bool,2> onThePlane);
+bool findInternalPoints(vector<Point3D>& intersectionPoints, double tol, vector<Point3D>& internalPoints, array<bool,2>& traceTips);
+vector<Trace> findTrace(const vector<Fracture>& fractures, double tol);
+void printTracesToFile(const vector<Trace>& traces, const string& filename);
+void sortAndDivideTracesByFracture(const vector<Trace>& traces, const string& filename);
+}
 #endif // UTILS_HPP
-
